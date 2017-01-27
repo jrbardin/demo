431ad077eb6a3d12539601e51ce228a946d12e00
jrbardin <jess.bardin@elicitinsights.com>
2017-01-27 09:45:39 -0600

    add rules to header

--- cars.R
+++ cars.R
@@ -9,6 +9,10 @@
 #   This script helps narrow down best car options from the mtcars data set
 #   included in the 'datasets' package
 #
+#   Rules:
+#   + stick shifts only
+#   + at least 200 horses
+#   + prefer >= 18 mpg and 6 or more cylinders
 #
 # Legal Notice:
 #   All rights in this program are owned by Elicit, LLC.  
